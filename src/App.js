3640c82ed8d90355bac215abcbf9237392d99d7f
jonfreed256 <github email address>
2019-10-30 13:48:50 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { hot } from 'react-hot-loader/root';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1> Hello, World! </h1>
+      </div>
+    );
+  }
+}
+
+export default hot(App);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+ReactDOM.render(<App />, document.getElementById('root'));
