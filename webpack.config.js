4bb05b680900aff1be2a9a8a1edd0ff224c0e0b3
jonfreed256 <github email address>
2019-12-10 13:44:51 -0500

    [UPDATED] webpack config, babelrc, package.json - [ADDED] eslintrc, prettier json

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devtool: 'eval',
   module: {
     rules: [
       {
@@ -28,7 +29,8 @@ module.exports = {
     contentBase: path.join(__dirname, 'public/'),
     port: 3001,
     publicPath: 'http://localhost:3000/dist/',
-    hotOnly: true
+    hotOnly: true,
+    historyApiFallback: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
 };
